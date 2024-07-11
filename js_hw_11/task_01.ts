73358b651c702e53fd3486d2e6d347950e591ddf
Maximus2806 <marozaumaksim@gmail.com>
2024-07-11 16:31:23 +0300

    ts_hw3 'Utility types' task_01, task_02

---
+++ js_hw_11/task_01.ts
@@ -0,0 +1,47 @@
+export{};
+// 1. Создайте интерфейс IEmployee {name: string, salary: number, isManager: boolean } и объект QA: IEmployee
+
+interface IEmployee {
+    name: string, 
+    salary: number, 
+    isManager: boolean, 
+};
+
+const QA: IEmployee = {
+    name: "Jhon",
+    salary: 2000,
+    isManager: false
+};
+
+// 2. Создайте тип EmployeeKeys, который будет юнионом названий ключей IEmployee (использовать keyof)
+
+type EmployeeKeys = keyof IEmployee;
+
+// 3. Создайте тип QaKeys, который будет содержать лишь ключи объекта QA(использовать keyof и typeof)
+
+type QaKeys = keyof typeof QA;
+
+// 4. Создайте тип UserType из объекта QA (используйте typeof)
+
+type UserType = typeof QA;
+
+// 5. Создайте тип, в котором все поля интерфейса IEmployee будут необязательными
+
+type EmployeeOptional = Partial<IEmployee>;
+
+// 6. Создайте тип, который будет содержать поля name и salary из интерфейса IEmployee
+
+type EmployeeNameSalary = Pick<IEmployee, 'name' | 'salary'>;
+
+// 7. Создайте тип, который будет держать все поля из интерфейса IEmployee, кроме isManager
+
+type EmployeeWithoutIsManager = Omit<IEmployee, 'isManager'>;
+
+// 8. Создайте тип, который будет содержать все поля из интерфейса IEmployee и сделает их неизменяемыми (readonly)
+
+type EmployeeReadOnly = Readonly<IEmployee>;
+
+// 9. Создайте тип, для массива объектов, где в ключах могут быть строки, в значениях number, string или boolean
+
+type ArrOfObj = {[key: string]: number | string | boolean}[]
+
---
+++ js_hw_11/task_02.ts
@@ -0,0 +1,81 @@
+// Создайте дженерик класс Storage<T>, где T должен быть ограничен объектом, имеющим КАК МИНИМУМ {id: number}.
+// Задача класса - хранить объекты типа Т в приватном массиве
+// Реализуйте в классе следующие методы:
+//   - constructor должен принимать необзятельный массив объектов соответствующего типа. 
+//     Если массив пришел - объекты запушить в хранилище.
+//   - add, принимающий либо объект типа Т, либо объект типа Т без id. Метод должен быть реализовать с помощью ПЕРЕГРУЗКИ.
+//     Если на вход подан объект без айди - айди надо сгенерировать, затем запушить обьект в хранилище
+//     Если на вход подан объект с айди - запушить его в хранилище
+//     Для типизации используйте Utility Types
+//   - update, принимающий объект с айди и любым набором остальных ключей из типа Т. 
+//   - remove, принимающий на вход id и удаляющий объект из массива
+//   - getById(id), возвращающий объект по айди если найден
+//   - getAll(), возвращает все объекты в хранилище
+
+type TObj = {
+    id: number
+};
+
+class MyStorage<T extends TObj>{
+    private data: T[] = [];
+    constructor(arr?: T[]){
+        if (arr) {
+            this.data.push(...arr)
+        }
+    };
+    private getNewId(objArr:T[]):number {
+        let newId = 0;
+        objArr.forEach((obj:T) => {
+            if (newId < obj.id) newId = obj.id            
+        });
+        return newId + 1;
+    };
+
+    add(obj: T): void;
+    add(obj: Omit<T, "id">): void;
+    add(obj: T | Omit<T, "id">){
+        if(!("id" in obj)){
+            const id = this.getNewId(this.data);
+            const newObj = {...obj, id}
+            this.data.push(newObj as T)
+        } else {            
+            this.data.push(obj)
+        }
+    };
+    update(obj: Pick<T, "id"> & Partial<T>):void{
+        const index = this.data.findIndex((item) => item.id == obj.id);
+        if (index !== -1) {            
+            const currentObj = this.data[index];
+            const updatedObj = {...currentObj, ...obj};
+            this.data.splice(index, 1, updatedObj);
+        }
+        
+    };
+    remove(id:number):void {
+        const index = this.data.findIndex((item) => item.id == id);
+        if (index !== -1) {
+            this.data.splice(index, 1);
+        }        
+    };
+    getById(id:number):T{
+        const result = this.data.find((item) => item.id === id);
+        if (result !== undefined){
+            return result
+        } else throw new Error (`Object with id = ${id} was not found`);        
+    };
+    getAll():T[]{
+        return this.data
+    }
+};
+
+const users = [
+    { id: 1, name: "Max", email: "max_78@gmail.com" },
+    { id: 2, name: "Sveta", email: "sv_7@gmail.com" }
+];
+const userForUpdate = { id: 1, name: "Max", surname: "Marozau", email: "max_78@gmail.com" }
+const newUser = {name: "Ivan", surname: "Petrov", email: "iv@gmail.com", phone: "434335245" }
+const testStorage = new MyStorage(users);
+testStorage.add(newUser);
+testStorage.update(userForUpdate);
+testStorage.remove(3);
+console.log(testStorage.getAll());
